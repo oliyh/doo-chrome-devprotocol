1bc5a4f109e2be887949257ac2ef562365ebe13d
Oliver Hine <git@oliy.co.uk>
2018-07-11 15:33:42 +0100

    Final preparations for first release

--- project.clj
+++ project.clj
@@ -1,18 +1,26 @@
 (defproject doo-chrome-devprotocol "0.1.0-SNAPSHOT"
   :description "A doo runner for Chrome using the Chrome Dev Protocol on the JVM"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/oliyh/doo-chrome-devprotocol"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [io.webfolder/cdp4j "3.0.1"]
+                 [io.webfolder/cdp4j "3.0.2"]
                  [org.clojure/tools.logging "0.4.1"]]
+  :release-tasks [["vcs" "assert-committed"]
+                  ["change" "version" "leiningen.release/bump-version" "release"]
+                  ["vcs" "commit"]
+                  ["vcs" "tag" "--no-sign"]
+                  ["deploy" "clojars"]
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["vcs" "commit"]
+                  ["vcs" "push"]]
   :java-source-paths ["src"]
   :javac-options ["-target" "1.8" "-source" "1.8"]
-  :plugins [[lein-doo "0.1.8"]
+  :plugins [[lein-doo "0.1.10"]
             [lein-cljsbuild "1.1.7"]]
   :profiles {:dev {:resource-paths ["test-resources"]
-                   :dependencies [[org.clojure/clojurescript "1.9.946"]
-                                  [lein-doo "0.1.8"]]}}
+                   :dependencies [[org.clojure/clojurescript "1.10.339"]
+                                  [lein-doo "0.1.10"]]}}
 
   :cljsbuild {:builds [{:id "passing-tests"
                         :source-paths ["test-resources/src"]
